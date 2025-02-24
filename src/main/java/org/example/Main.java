640ffae4895f46c13e38d544aa33575439ec242c
jayp1206 <jpatel1206@gmail.com>
2025-02-24 10:28:50 -0500

    wrote program to convert a given number of gallons to quarts, pints, cups, and tablespoons

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,1 +1,24 @@
 package org.example; //DO NOT DELETE
+
+import java.util.Scanner;
+
+/*
+ * Project Name: Gallon Convertor
+ * Author: Jay Patel
+ */
+
+public class Main {
+    public static void main (String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter the number of gallons: ");
+    double gallons = input.nextDouble();
+    
+    System.out.println("In " + gallons + " gallons there are: ");
+    System.out.println((gallons * 4) + " quarts");
+    System.out.println((gallons * 8) + " pints");
+    System.out.println((gallons * 16) + " cups");
+    System.out.println((gallons * 256) + " tablespoons");
+
+    }
+}
